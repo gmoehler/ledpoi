54614f012c7dc592fdfa715a3ac6ac263cce74ae
Gregor Moehler <gmoeh@gmx.de>
2017-04-25 09:12:31 +0200

    remove non req stuff

--- src/pixelgateway.cpp
+++ src/pixelgateway.cpp
@@ -59,8 +59,6 @@ uint8_t progState=0;
 PoiState poiState = POI_INIT;
 PoiState nextPoiState = POI_INIT;
 
-
-
 void printLine()
 {
   Serial.println();
@@ -84,48 +82,6 @@ void printWifiStatus() {
   Serial.print("signal strength (RSSI):");
   Serial.print(rssi);
   Serial.println(" dBm");
-}
-
-void requestURL(const char * host, uint8_t port)
-{
-  printLine();
-  Serial.println("Connecting to domain: " + String(host));
-
-  // Use WiFiClient class to create TCP connections
-  WiFiClient client;
-  if (!client.connect(host, port))
-  {
-    Serial.println("connection failed");
-    return;
-  }
-  Serial.println("Connected!");
-  printLine();
-
-  // This will send the request to the server
-  client.print((String)"GET / HTTP/1.1\r\n" +
-  "Host: " + String(host) + "\r\n" +
-  "Connection: close\r\n\r\n");
-  unsigned long timeout = millis();
-  while (client.available() == 0)
-  {
-    if (millis() - timeout > 5000)
-    {
-      Serial.println(">>> Client Timeout !");
-      client.stop();
-      return;
-    }
-  }
-
-  // Read all the lines of the reply from server and print them to Serial
-  while (client.available())
-  {
-    String line = client.readStringUntil('\r');
-    Serial.print(line);
-  }
-
-  Serial.println();
-  Serial.println("closing connection");
-  client.stop();
 }
 
 void displayOff() {
@@ -152,93 +108,6 @@ void statusIO() {
 void statusNIO() {
   pixels[0] = makeRGBVal(33, 0, 0);
   ws2812_setColors(1, pixels);
-}
-
-void scanner(unsigned long delay_ms, unsigned long timeout_ms) {
-  int currIdx = 0;
-  int prevIxd = 0;
-  bool RUN_FOREVER = (timeout_ms == 0 ? true : false);
-  unsigned long start_ms = millis();
-  while (RUN_FOREVER || (millis() - start_ms < timeout_ms)) {
-    pixels[prevIxd] = makeRGBVal(0, 0, 0);
-    pixels[currIdx] = makeRGBVal(MAX_COLOR_VAL, MAX_COLOR_VAL, MAX_COLOR_VAL);;
-    ws2812_setColors(NUM_PIXELS, pixels);
-    prevIxd = currIdx;
-    currIdx++;
-    if (currIdx >= NUM_PIXELS) {
-      currIdx = 0;
-    }
-    delay(delay_ms);
-  }
-}
-
-void rainbow(unsigned long delay_ms, unsigned long timeout_ms)
-{
-  const uint8_t color_div = 4;
-  const uint8_t anim_step = 1;
-  const uint8_t anim_max = MAX_COLOR_VAL - anim_step;
-  rgbVal color = makeRGBVal(anim_max, 0, 0);
-  rgbVal color2 = makeRGBVal(anim_max, 0, 0);
-  uint8_t stepVal = 0;
-  uint8_t stepVal2 = 0;
-
-  bool RUN_FOREVER = (timeout_ms == 0 ? true : false);
-  unsigned long start_ms = millis();
-  while (RUN_FOREVER || (millis() - start_ms < timeout_ms)) {
-    color = color2;
-    stepVal = stepVal2;
-
-    for (uint16_t i = 0; i < NUM_PIXELS; i++) {
-      pixels[i] = makeRGBVal(color.r/color_div, color.g/color_div, color.b/color_div);
-
-      if (i == 1) {
-        color2 = color;
-        stepVal2 = stepVal;
-      }
-
-      switch (stepVal) {
-        case 0:
-        color.g += anim_step;
-        if (color.g >= anim_max)
-        stepVal++;
-        break;
-
-        case 1:
-        color.r -= anim_step;
-        if (color.r == 0)
-        stepVal++;
-        break;
-
-        case 2:
-        color.b += anim_step;
-        if (color.b >= anim_max)
-        stepVal++;
-        break;
-
-        case 3:
-        color.g -= anim_step;
-        if (color.g == 0)
-        stepVal++;
-        break;
-
-        case 4:
-        color.r += anim_step;
-        if (color.r >= anim_max)
-        stepVal++;
-        break;
-
-        case 5:
-        color.b -= anim_step;
-        if (color.b == 0)
-        stepVal = 0;
-        break;
-      }
-    }
-
-    ws2812_setColors(NUM_PIXELS, pixels);
-
-    delay(delay_ms);
-  }
 }
 
 void blink(int m){
@@ -338,12 +207,13 @@ void wifi_connect(){
   WiFi.config(myIP,gateway,subnet);
    while (!connectedToWifi){
      WiFi.begin(WIFI_SSID, WIFI_PASS);
-     Serial.println("Connecting...");
+     Serial.print("Connecting...");
 
      while (WiFi.status() != WL_CONNECTED) {
        // Check to see if connecting failed.
        // This is due to incorrect credentials
        delay(500);
+       Serial.print(".");
      }
      if (WiFi.status() == WL_CONNECT_FAILED) {
        Serial.println("Connection Failed. Retrying...");
@@ -353,12 +223,13 @@ void wifi_connect(){
        blink(10);
        connectedToWifi=1;
        Serial.println("Connected.");
+       printWifiStatus();
      }
    }
 
  //  printWifiStatus();
    digitalWrite(LED_PIN, LOW); // Turn off LED
-   server.begin();    // das hier ist WICHTIG!!
+   server.begin();    // important
 }
 
 
