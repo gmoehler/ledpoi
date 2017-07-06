d2353fd23291f2c249e155143c25178e2124ff31
Gregor Moehler <gmoeh@gmx.de>
2017-07-06 22:52:18 +0200

    add comments

--- src/handler/AbstractHandler.h
+++ src/handler/AbstractHandler.h
@@ -9,15 +9,28 @@
 class AbstractHandler
 {
 public:
+  // returns a string identifier for messages
   virtual const char* getActionName() = 0;
 
+   // called for next step
+   // may switch handler from active to inactive
+   // if it is already inactive it will do nothing
   virtual void next() = 0;
+  
+  // check whether action is still active (runming)
+  // or inactive (finished)
   virtual bool isActive() = 0;
   
+  // delay configured beteeen 2 steps
+  // delay is enforced outside of handler
   virtual uint16_t getDelayMs() = 0;
+  
+  // array of size N_PIXELS with the current colors
   virtual rgbVal* getDisplayFrame() = 0;
 
+  // general info about the action
   virtual void printInfo() = 0;
+  // info about current state of the action
   virtual void printState() = 0;
 };
 #endif
